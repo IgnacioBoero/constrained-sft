a59cb27ea7acc2f60239cc3537078c4e9bea7e00
iboero <iboero@seas.upenn.edu>
2026-01-27 13:50:14 -0500

    pen

--- src/train.py
+++ src/train.py
@@ -121,7 +121,8 @@ def main(cfg: DictConfig):
             if cfg.train.do_initial_eval:
                 trainer.evaluate(metric_key_prefix="eval")
             trainer.train()
-    if cfg.train.eval_at_end:
+    eval_at_end = getattr(cfg.train, "eval_at_end", False)
+    if eval_at_end:
         trainer._current_eval_prefix = "eval"
         trainer.evaluate(metric_key_prefix="eval")
     # Finish wandb run
